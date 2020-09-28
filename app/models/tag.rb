588212310e27a0dfcc6faddcf3c5c2f42336f8e9
arslonga-vitabrevis <croquis.marble@gmail.com>
2020-09-28 20:38:05 +0900

    タグ名に一意性制約を設ける

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
   has_many :tweet_tag_relations
   has_many :tweets, through: :tweet_tag_relations
+
+  #一意性制約はFormオブジェクトではなくモデルに記載
+  validates :name, uniqueness: true
 end
